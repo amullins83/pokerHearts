d1936201308b42e2adcb341af46c42337c02ec5e
Austin Mullins <austin.mullins@gmail.com>
2013-05-09 12:45:33 -0500

    Select boxes update correctly. finally.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -15,22 +15,45 @@
   WordCountCtrl = (function() {
     function WordCountCtrl($scope, $http) {
 		$scope.text = window.localStorage["text"];
-		
-		var assignmentOption = $("#assignment option").filter(function() {
-			return $(this).text() === JSON.parse(localStorage["assignment"]).name;
-		});
-		
-		assignmentOption.prop("selected",true);
-		
-		$scope.selectedAssignment;
-		
-		
-		$("#submission option").filter(function() {
-			return $(this).text() === localStorage["submission"];
-		}).prop("selected", true);
-		
 		$scope.submissionTypes = ["Post 1", "Post 2", "Post 3", "Reflection"];
-	  
+
+    setTimeout(function() {
+      console.log("Ready!");
+		  var assignmentOption = $("#assignment option").filter(function() {
+        if(typeof(window.localStorage["assignment"]) === "undefined") {
+          console.log("I couldn't find the assignment local store");
+          return false;
+        }
+        console.log("The local assignment is: " + window.localStorage["assignment"]);
+        console.log("The option I'm looking at is: " + $(this).text());
+			 return $(this).text() === JSON.parse(window.localStorage["assignment"]).name;
+		  });
+		
+      console.log("I found " + assignmentOption.length + " matching assignments");
+      if(assignmentOption.length > 0) {
+        console.log("I'm changing the value of the assignment select to " + assignmentOption.val());
+        $("#assignment").val(assignmentOption.val());
+        console.log("done!");
+        console.log("I'm changing the selectedAssignment in scope to " + JSON.parse(window.localStorage["assignment"]));
+        $scope.selectedAssignment = JSON.parse(window.localStorage["assignment"]);
+        console.log("done!");
+      }
+		
+      var $chosenSubmission = $("#submission option").filter(function() {
+        console.log("localStorage['submission'] = " + window.localStorage["submission"]);
+        console.log("This option = " + $(this).text());
+			 return $(this).text() === window.localStorage["submission"];
+		  });
+		
+      console.log("I found " + $chosenSubmission.length + " matches");
+      console.log("Setting scope.selectedSubmission = " + $chosenSubmission.val());
+      $scope.selectedSubmission = $chosenSubmission.val();
+      console.log("done!");
+      console.log("Setting value of the submission select to " + $scope.selectedSubmission);
+      $("#submission").val($scope.selectedSubmission);
+      console.log("done!");
+	  }, 500);
+
 		$(document).on("keyup", "textarea", function() {
 			window.localStorage["text"] = $("textarea").val();
 			if(typeof($scope.selectedAssignmentJSON) !== "undefined")
@@ -59,31 +82,58 @@
 		});
 
 		$(document).on("change", "#assignment", function(e) {
-			for(var i = 0; i < $scope.assignments.length; i++) {
-				if($scope.selectedAssignment.name === $scope.assignments[i].name) {
-					$scope.assignments[i] = $scope.selectedAssignment;
-					break;
-				}
-			}
+      if(typeof($scope.selectedAssignment) !== "undefined") {
+        for(var i = 0; i < $scope.assignments.length; i++) {
+          if($scope.selectedAssignment.name === $scope.assignments[i].name) {
+            $scope.assignments[i] = $scope.selectedAssignment;
+            break;
+				  }
+        }
+      }
 			
 			var $assignment = $(e.target);
+      var $submission = $("#submission");
 			window.localStorage["assignment"] = $assignment.val();
 			
 			$scope.selectedAssignment = JSON.parse($assignment.val());
-		
-		    if($("#submission").val() !== "") {
-				if($("#submission").val() === "Reflection")
-					$scope.text = $scope.selectedAssignment.reflectText;
-				else {
-					if(typeof($scope.selectedAssignment.postText) === "undefined")
-						$scope.selectedAssignment.postText = [];
-					$scope.text = $scope.selectedAssignment.postText[parseInt($("#submission").val().match(/(\d+)/)) - 1];
-				}
-			}
-			
-			
+		  $scope.selectedSubmission = $submission.val();
 
+		  if(typeof($scope.selectedSubmission) !== "undefined" && $scope.selectedSubmission !== "") {
+		  	if($scope.selectedSubmission === "Reflection")
+			 		$scope.text = $scope.selectedAssignment.reflectText;
+        else {
+					if(typeof($scope.selectedAssignment.postText) === "undefined")
+            $scope.selectedAssignment.postText = [];
+          $scope.text = $scope.selectedAssignment.postText[parseInt($scope.selectedSubmission.match(/(\d+)/)) - 1];
+        }
+        $("textarea").val($scope.text);
+        $("textarea").keyup();  
+      }
+		
+      
 		});
+
+    $(document).on("change", "#submission", function(e) {
+      var $submission = $(e.target);
+      var $assignment = $("#assignment");
+      window.localStorage["submission"] = $submission.val();
+
+      $scope.selectedAssignment = JSON.parse($assignment.val());
+      $scope.selectedSubmission = $submission.val();
+
+      if(typeof($scope.selectedAssignment) !== "undefined") {
+          if($scope.selectedSubmission === "Reflection")
+            $scope.text = $scope.selectedAssignment.reflectText;
+          else {
+           if(typeof($scope.selectedAssignment.postText) === "undefined")
+              $scope.selectedAssignment.postText = [];
+           $scope.text = $scope.selectedAssignment.postText[parseInt($scope.selectedSubmission.match(/(\d+)/)) - 1];
+          }
+
+        $("textarea").val($scope.text);
+        $("textarea").keyup();
+      }
+    });
 
       $(document).on("click", "#go", function() {
         if(typeof($scope.selectedAssignment) !== "undefined") {
@@ -104,9 +154,9 @@
         method: 'GET',
         url: '/api/assignments'
       }).success(function(data, status, headers, config) {
-        return $scope.assignments = data;
+        $scope.assignments = data;
       }).error(function(data, status, headers, config) {
-        return $scope.asssignments = [{name: "Error", date:"Never"}];
+        $scope.asssignments = [{name: "Error", date:"Never"}];
       });
     }
 
